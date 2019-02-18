b9ddd4ddf938857e249a8346077c9cc10d392744
MariusHull <marius.hullin@gmail.com>
2019-02-18 14:40:00 +0100

    Added Data
    Added the data schemes, models and the routes for answers, question, users

---
+++ routes/questions.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Question = require('../models/Question');
+
+/* GET ALL Questions */
+
+router.get('/', function(req, res, next) {
+  Question.find(function (err, questions) {
+    if (err) return next(err);
+    res.json(questions);
+  });
+});
+
+
+/* GET SINGLE Question BY ID */
+router.get('/:id', function(req, res, next) {
+  Question.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* SAVE Question */
+router.post('/', function(req, res, next) {
+  Question.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* UPDATE Question */
+router.put('/:id', function(req, res, next) {
+  Question.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE Question */
+router.delete('/:id', function(req, res, next) {
+  Question.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+module.exports = router;
