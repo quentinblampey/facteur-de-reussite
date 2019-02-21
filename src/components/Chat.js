0a557df6033141c1fcc1b1349a43bdf61e940869
Nicolas GABRION <nico.gabrion@gmail.com>
2019-02-21 10:07:34 +0100

    -

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -30,18 +30,22 @@ class Chat extends Component {
   }
 
   onSubmitButton = (answer, e) => {
-    this.state.chat.push(answer.body);
-    this.state.chat.push(answer.reaction);
+    this.setState({chat:this.state.chat.concat(answer.body, answer.reaction)})
     console.log('heyo');
     axios.post(`/api/answers/${this.state.user._id}`, {answer:answer, field:this.state.currentQuestion.field})
       .then(res => {
-        console.log(res);
-        axios.post(`/api/questions/${this.state.user._id}`)
-          .then(res2 => {
-            if (res2.body.isFinish){this.props.history.push(`/`);};
-            console.log(res2);
-            this.setState({user:res2.data.user, chat:this.state.chat.concat([res2.data.question.body]), currentQuestion:res2.data.question})
-          });
+        if (answer.breakPoint){this.props.history.push("/");}
+        else{
+          console.log(res);
+          axios.post(`/api/questions/${this.state.user._id}`)
+            .then(res2 => {
+              if (res2.body.isFinish){this.props.history.push("/");}
+              else{
+                console.log(res2);
+                this.setState({user:res2.data.user, chat:this.state.chat.concat([res2.data.question.body]), currentQuestion:res2.data.question})
+              }
+            });
+        }
       });
   }
 
@@ -51,15 +55,19 @@ class Chat extends Component {
     ans=this.state.currentQuestion.answers[0];
     ans.body=this.state.newMessage;
     ans.detail=this.state.newMessage;
+    console.log(this.state.currentQuestion);
+    console.log(ans, this.state.currentQuestion.field);
     axios.post(`/api/answers/${this.state.user._id}`, {answer:ans, field:this.state.currentQuestion.field})
       .then(res => {
+        console.log(res);
         axios.post(`/api/questions/${this.state.user._id}`)
           .then(res2 => {
-            if (res2.body.isFinish){console.log('t');this.props.history.push(`/`);};
-            this.state.chat.push(res2.data.question.body);
+            console.log(res2);
+            if (res2.data.isFinish){console.log('t');this.props.history.push(`/`);};
             this.setState({chat:this.state.chat.concat([res2.data.question.body]), currentQuestion:res2.data.question});
           });
       });
+      
   }
   render() {
     const { chat, user, newMessage, currentQuestion} = this.state;
@@ -73,10 +81,10 @@ class Chat extends Component {
     }
     else{
       userAnswer = (
-        <form onSubmit={this.onSubmit}>
+        <>
           <input type="text" class="form-control" name="newMessage" value={newMessage} onChange={this.onChange} placeholder="..." />
-          <button type="submit" class="btn btn-default">Envoyer</button>
-        </form>
+          <button type="submit" onClick={this.onSubmit.bind(this)} class="btn btn-default">Envoyer</button>
+        </>
       )
     }
     return (
